9a4405e97299f5ae40dd0dab036bb3d32b3f4e28
kelvinguchu <kelvinguchu5@gmail.com>
2025-09-18 00:56:05 +0300

    refactor: update Content Security Policy and hostname in next.config.mjs
    - Adjusted the Content Security Policy to improve security and resource loading.
    - Updated the hostname for the remote image configuration to reflect the new deployment.

--- next.config.mjs
+++ next.config.mjs
@@ -33,12 +33,12 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-                        value:
+            value:
               "frame-ancestors 'self'; " +
               "frame-src 'self' https://www.googletagmanager.com https://*.doubleclick.net https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.google.com https://pagead2.googlesyndication.com https://fundingchoicesmessages.google.com https://*.google.com https://www.scoreaxis.com https://scoreaxis.com https://*.scoreaxis.com; " +
               "connect-src 'self' https://*.google-analytics.com https://www.googletagmanager.com https://www.google.com https://*.google.com https://*.doubleclick.net https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://fundingchoicesmessages.google.com https://ep1.adtrafficquality.google https://ep2.adtrafficquality.google https://api.open-meteo.com https://pro-api.coinmarketcap.com; " +
               "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://www.gstatic.com https://tpc.googlesyndication.com https://pagead2.googlesyndication.com https://securepubads.g.doubleclick.net https://googleads.g.doubleclick.net https://fundingchoicesmessages.google.com https://ep2.adtrafficquality.google; " +
-              "img-src * data: blob:; " +
+              'img-src * data: blob:; ' +
               "style-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.gstatic.com; ",
           },
         ],
@@ -74,7 +74,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'dawan.so.vercel.app',
+        hostname: 'dawanso-two.vercel.app',
       },
     ],
     formats: ['image/webp', 'image/avif'],
